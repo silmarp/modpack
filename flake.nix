0fa546549dd6a9ca6e1a00166760a580ee954039
Silmar <silmarjr2@gmail.com>
2025-08-30 13:30:17 -0300

    add flake and envrc

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Silmar's modpack";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      inherit (builtins) attrValues;
+      pkgs = import nixpkgs { inherit system; };
+    in
+    rec {
+      devShells = rec {
+        modpack = pkgs.mkShell {
+          name = "modpack";
+          nativeBuildInputs = with pkgs; [ packwiz unzip zip ];
+        };
+        default = modpack;
+      };
+    });
+}
